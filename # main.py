4731d1fc6fada2be0abb121e30e3e19624a38383
Shivang Raj Saxena <shivangrajsaxena6@gmail.com>
2024-08-25 17:56:23 +0530

    Add files via upload

---
+++ # main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello_azure():
+    return {"message": "Hello, Azure Web Apps!"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8080)
